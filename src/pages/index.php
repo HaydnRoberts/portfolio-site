bf352cb21ca7fe1862bf41feb54eb25a7665526f
HaydnRoberts <hr342208@truro-penwith.ac.uk>
2024-03-21 23:24:50 +0000

    Information page updated

--- src/pages/index.php
+++ src/pages/index.php
@@ -7,9 +7,31 @@
     <title>Haydn Roberts' portfolio</title>
 </head>
 <body>
-    <h1>
-        Hello world!
-    </h1>
-    <p class="text-3xl font-bold underline text-eastern-blue-500">test</p>
+    <div class="flex bg-light-eastern-blue-300 h-96">
+        <div class="m-auto">
+            <p class="text-5xl font-bold text-eastern-blue-700 p-4">
+                Haydn Roberts
+            </p>
+            <p class="text-3xl text-eastern-blue-700 px-4">
+                Junior Developer
+            </p>
+        </div>
+        <div class="m-auto">
+
+        </div>
+    </div>
+    <div class="flex bg-light-eastern-blue-900 h-96">
+        <div class="m-auto">
+        
+        </div>
+        <div class="m-auto">
+            <p class="text-4xl font-bold text-eastern-blue-700 p-4">
+                About me
+            </p>
+            <p class="text-2xl text-eastern-blue-700 px-4 max-w-screen-sm">
+                Hello, I'm Haydn Roberts, a Junior developer from Cornwall studying Digital design production and development at Truro college. I am doing a work placement at Tango 3 and I have been working at Kennack sands beach cafe for 2 years.
+            </p>
+        </div>
+    </div>
 </body>
 </html>
