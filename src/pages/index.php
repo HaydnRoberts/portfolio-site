e3fd324b572dcfe19a3b2a9892e4c6d9264303f1
HaydnRoberts <hr342208@truro-penwith.ac.uk>
2024-03-22 14:20:08 +0000

    Additional content added

--- src/pages/index.php
+++ src/pages/index.php
@@ -38,12 +38,54 @@
             <p class="text-4xl font-bold text-eastern-blue-700 p-4">
                 Experience
             </p>
-            <p class="text-2xl text-eastern-blue-700 px-4">
-                ...
-            </p>
+            <div class="text-2xl text-eastern-blue-700 px-4">
+                <p>Present</p>
+                <p class="font-bold pb-2">Barista/Cashier/Waiter, Kennack sands beach cafe</p>
+                <p>20th September 2022 – 19th October 2022</p>
+                <p class="font-bold pb-2">Volunteer playtester, Behaviour interactive</p>
+                <p>2021</p>
+                <p class="font-bold pb-2">Work experience, rocketmakers</p>
+                <p>6th March 2023 – 23rd March 2023</p>
+                <p class="font-bold pb-2">Internal work Placement, Truro college</p>
+                <p>19th June 2023 – 7th July 2023</p>
+                <p class="font-bold pb-2">Work Placement block 1, Tango 3</p>
+                <p>18th September 2023 – 6th October 2023</p>
+                <p class="font-bold pb-2">Work placement block 2, Tango 3</p>
+                
+            </div>
         </div>
         <div class="m-auto">
 
+        </div>
+    </div>
+    <div class="flex bg-slate-200 h-128">
+        <div class="m-auto">
+            
+        </div>
+        <div class="m-auto">
+
+            <p class="text-4xl font-bold text-eastern-blue-700 p-4">
+                Work for clients
+            </p>
+            <p class="text-2xl text-eastern-blue-300 px-4 underline">    
+                <a href="https://lanjeth-b6bf8.web.app/">Lanjeth website</a>
+            </p>
+            <p class="text-2xl text-eastern-blue-300 px-4 underline">    
+                <a href="https://career-coach-brad-online.web.app/">Career coach AI</a>
+            </p>
+            <p class="text-4xl font-bold text-eastern-blue-700 p-4">
+                Personal work
+            </p>
+            <p class="text-2xl text-eastern-blue-300 px-4 underline">    
+                <a href="https://github.com/HaydnRoberts">My Github</a>
+            </p>
+            <p class="text-4xl font-bold text-eastern-blue-700 p-4">
+                Early work
+            </p>
+            <p class="text-2xl text-eastern-blue-300 px-4 underline">    
+                <a href="https://replit.com/@bighatbenny">Repl</a> 
+            </p>
+                
         </div>
     </div>
 </body>
